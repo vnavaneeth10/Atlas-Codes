57c147d8dba5f8a6a17771ace3ad9c21727f09bc
Navaneeth V <navaneethnair69@gmail.com>
2022-03-11 14:20:53 +0530

    commit

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,12 @@
+public class InvertedHalfPyramid {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+    
+}
---
+++ InvertedHalfPyramid180.java
@@ -0,0 +1,21 @@
+public class InvertedHalfPyramid180 {
+    public static void main(String[] args) {
+        int n = 4;
+     for(int i=1; i<=n; i++){
+         //inner loop -> space print
+         for(int j=1; j<=n-i; j++){
+             System.out.print(" ");
+         }
+
+         //inner loop -> start print
+         for( int j=1; j<=i; j++ ){
+             System.out.print("*");
+         }
+
+         System.out.println();
+     
+        }
+    
+    }
+    
+}
