d90184a40dd294cbf895110a954d76de92178b9a
Navaneeth V <navaneethnair69@gmail.com>
2022-03-11 13:10:58 +0530

    commit

---
+++ StarsSolidRectangle.java
@@ -0,0 +1,16 @@
+public class StarsSolidRectangle {
+
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=4; i++){ //outer loop
+            
+            for(int j=1; j<=5; j++) { //inner loop
+                
+                System.out.print("*");
+            }
+           
+            System.out.println("");
+        }
+    }
+    
+}
